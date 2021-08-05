fa75fa9b54738d5bd7627df2df39812f1bc71a20
Tw0ch <egormizulin@mail.ru>
2021-08-05 14:21:02 +0300

    Update dart_application_1.dart

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,5 +1,7 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+void main() {
+  sum(5, 7);
+}
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+void sum(a, b) {
+  print(a + b);
 }
