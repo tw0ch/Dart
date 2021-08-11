69d9358fc5e7b1021b5ba8cf245a98f45bfc2426
Tw0ch <egormizulin@mail.ru>
2021-08-11 18:10:35 +0300

    Update dart_application_1.dart
    1st day learn OOP

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,22 +1,7 @@
-import 'dart:typed_data';
-
-const int num = 10;
-var world = true;
-
 void main() {
-  double point;
-  point = 4.5;
-  point++;
 
-  print('Твой результат по окончанию теста: ${point.sign} балл(а) $num');
 
-  dynamic dynamicData = 5;
-  dynamicData = 'hi';
-  dynamicData = false;
-  print(dynamicData);
-
-  var a = 15, b = 5;
-  print('+ : ${a ~/ b}');
- 
-  ;
 }
+
+class
+
